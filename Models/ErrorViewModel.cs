c9d4d3a6fb588a3b6bac8a01e03a11f5458f1239
Rafael Mesquita <rafaelempresarialacc@gmail.com>
2021-02-15 15:43:14 -0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Formularios_bia.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
